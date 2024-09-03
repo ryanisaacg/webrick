a95dad17e1f1f054e44ebbabc790e6d149ef3066
Ryan <ryanisaacg@users.noreply.github.com>
2024-09-03 11:51:10 -0400

    Set up crate to compile to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,51 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use brick::SourceFile;
+use brick_ld::InputModule;
+use brick_wasm_backend::{compile, BackendOptions};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use wasm_bindgen::prelude::wasm_bindgen;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[wasm_bindgen]
+pub fn compile_source(source: &str) -> Vec<u8> {
+    let sources = &[SourceFile {
+        filename: "<web input>",
+        module_name: "main",
+        contents: source.to_string(),
+    }];
+
+    match compile(
+        sources,
+        BackendOptions {
+            include_start_marker: true,
+            top_level_name: "main",
+        },
+    ) {
+        Ok(module) => {
+            match brick_ld::link(&[
+                InputModule {
+                    name: "main",
+                    definition: module.as_slice(),
+                    public_exports: true,
+                    is_start: true,
+                },
+                InputModule {
+                    name: "brick-runtime",
+                    definition: runtime_binary::wasm_runtime(),
+                    public_exports: false,
+                    is_start: false,
+                },
+            ]) {
+                Ok(module) => {
+                    return module;
+                }
+                Err(err) => {
+                    println!("internal compiler error: {err}");
+                }
+            }
+        }
+        Err(e) => {
+            println!("{e}");
+        }
     }
+
+    Vec::new()
 }
