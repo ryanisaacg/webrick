77b0eed5b8b8ae764a7ae1b534dae17062403450
Ryan <ryanisaacg@users.noreply.github.com>
2024-09-03 13:45:23 -0400

    Make typescript happy

--- site/src/main.ts
+++ site/src/main.ts
@@ -4,18 +4,19 @@ import { EditorView, basicSetup } from "codemirror";
 
 let view = new EditorView({
   extensions: [basicSetup],
-  parent: document.getElementById("editor"),
+  parent: document.getElementById("editor")!,
 });
 
-const outputElement = document.getElementById("output");
+const outputElement = document.getElementById("output")!;
 
-document.getElementById("build").addEventListener("click", () => {
+document.getElementById("build")!.addEventListener("click", () => {
   const { binary, errors } = compile_source(view.state.doc.toString());
   if (binary != null) {
     WebAssembly.instantiate(binary, {}).then((x) => {
-      outputElement.innerText = x.instance.exports.main();
+      outputElement.innerText = (x.instance.exports.main as CallableFunction)();
     });
   } else {
-    outputElement.innerText = errors;
+    outputElement.innerText =
+      errors ?? "No output and no errors - this is probably an ICE";
   }
 });
