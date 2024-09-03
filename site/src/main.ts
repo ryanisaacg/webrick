30b5af27c307672ab356ddc044cdde324862d5c0
Ryan <ryanisaacg@users.noreply.github.com>
2024-09-03 13:33:10 -0400

    Basic codemirror setup

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,6 +1,17 @@
 import { compile_source } from "webrick";
 
-const binary = compile_source("1 + 2 * 3");
-WebAssembly.instantiate(binary, {}).then((x) =>
-  console.log(x.instance.exports.main())
-);
+import { EditorView, basicSetup } from "codemirror";
+
+let view = new EditorView({
+  extensions: [basicSetup],
+  parent: document.getElementById("editor"),
+});
+
+const outputElement = document.getElementById("output");
+
+document.getElementById("build").addEventListener("click", () => {
+  const binary = compile_source(view.state.doc.toString());
+  WebAssembly.instantiate(binary, {}).then((x) => {
+    outputElement.innerText = x.instance.exports.main();
+  });
+});
